d07f2e03567f9fdb3f0efc2f8546fa3a18de9bb4
ananthujp <31985570+ananthujp@users.noreply.github.com>
2023-09-04 18:16:25 +0530

    Server- netlify

--- functions/api.js
+++ functions/api.js
@@ -1,21 +1,21 @@
 var express = require("express");
 const serverless = require("serverless-http");
 var nodemailer = require("nodemailer");
+require("dotenv").config();
 const app = express();
-const router = express.Router();
 app.listen(process.env.PORT || 3000);
 
 const client = nodemailer.createTransport({
   service: "Gmail",
   auth: {
-    user: "iitgn.me@gmail.com",
-    pass: "ssvcyprewayztovv",
+    user: "sagmayam@gmail.com",
+    pass: process.env.GMPASS,
   },
 });
 
 const sendMail = (email, code) => {
   client.sendMail({
-    from: "iitgn.me@gmail.com",
+    from: "sagmayam@gmail.com",
     to: email,
     subject: "Verification code",
     text: "Your verification code is :" + code,
@@ -25,12 +25,12 @@ const sendMail = (email, code) => {
 app.get("/send", (req, res) => {
   sendMail(req.query.email, req.query.code);
   //localhost:3000/send?code=2605&email=ananthujp@gmail.com
-  res.send("Success");
+  res.send(process.env.GMPASS);
 });
 
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: path.join(__dirname, "public") });
 });
-app.use("./netlify/functions/api", router);
+//module.exports = app;
 module.exports.handler = serverless(app);
